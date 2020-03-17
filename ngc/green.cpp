34143ebca6d231175be4370428e97010c14b0973
Ta Chen <tchen@perspectalabs.com>
2020-03-17 11:41:30 -0400

    orange and green

---
+++ ngc/green.cpp
@@ -0,0 +1,71 @@
+// pnt_example.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#ifdef _WIN32
+# include <Windows.h>
+#else
+# include <unistd.h>
+# include <string.h>
+#endif
+#include "pnt_data.h"
+#include "sensors.h"
+#include "ownship.h"
+#include "target.h"
+
+#include "rpc.h"
+
+bool orange_enclave = false;
+
+int main(int argc, char **argv)
+{
+   std::cout << "green" << std::endl;
+   
+  // Assume the color for p, d, v, vtgt is inferred from below coloring in constructors
+  // Touched on green side gpssensor constructor
+  Position p(.0, .0, .0); // initial position
+  // Touched on orange side by RFSensor constructor
+  Distance d(1062, 7800, 9000); // initial target distance
+  // Touched by green side gpssensor constructor
+  Velocity v(50, 25, 12);
+  // touched by orange side RFSensor constructor
+  Velocity vtgt(35, 625, 18);
+  #pragma cle begin GREEN
+  GpsSensor* gps = new GpsSensor(p, v);
+  #pragma cle end GREEN
+
+  #pragma cle begin ORANGE
+  RfSensor* rfs = new RfSensor(d, vtgt);
+    
+  OwnShip* uav = new OwnShip(100); // updates at 100 Hz frequency
+  #pragma cle end ORANGE
+  #pragma cle begin GREEN
+  Target* tgt = new Target (10); // updates at 10 Hz frequency
+  #pragma cle end GREEN
+
+  // setup the dataflow relationships
+  gps->attach(uav); // cross domain attach gps is green uav is orange
+  gps->attach(tgt);
+  uav->attach(tgt); // cross domain 
+  rfs->attach(tgt); // cross domain 
+  // _observers may be tained; contains a mix of local and remote observers; kind of split TBD
+
+  rpc_init(uav, tgt);
+  std::cout << "waiting for remote to be ready " << std::endl;
+  sleep(10);  // wait for remote end to be ready
+
+  while (true)
+    {
+      // here we simulate sensor data streams
+      gps->read();
+      rfs->read();
+      
+#ifdef _WIN32	  
+      Sleep(sleep_msec); // 100 Hz
+#else
+      usleep(sleep_msec * 1000);
+#endif
+    }
+  return 0;
+}
+
--- ngc/orange_target.cpp
+++ ngc/orange_target.cpp
@@ -12,18 +12,14 @@ void Target::update(Subject *s) {
   RfSensor *rf = dynamic_cast<RfSensor *>(s);
   if (uav) {
     setUAVLocation(uav->getPosition());
-    if (orange_enclave) {
-      // push UAV position to green via RPC
-      updateRemote(s);
-    }
+    // push UAV position to green via RPC
+    updateRemote(s);
   } else if (gps) {
     tick = true; // yeah.. hackish
   } else if (rf) {
     setDistance(rf->getDistance());
-    if (orange_enclave) {
-      // push the distance to green via RPC
-      updateRemote(s);
-    }
+    // push the distance to green via RPC
+    updateRemote(s);
   }
 		
   if (tick && _cycle != 0 && 0 == ++cnt % _cycle) {
