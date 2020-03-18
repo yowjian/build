b09d60be5ddd909c7627bbd7642c8b38eb79f321
Ta Chen <tchen@perspectalabs.com>
2020-03-17 20:28:34 -0400

    updated hal API

--- ngc/gma.h
+++ ngc/gma.h
@@ -20,11 +20,23 @@ typedef struct _position_datatype {
     double z;
 } position_datatype;
 
+typedef struct _position_output {
+    uint64_t  x;
+    uint64_t  y;
+    uint64_t  z;
+} position_output;
+
 typedef struct _distance_datatype {
-    double dx;
-    double dy;
-    double dz;
+    double x;
+    double y;
+    double z;
 } distance_datatype;
+
+typedef struct _distance_output {
+    uint64_t  x;
+    uint64_t  y;
+    uint64_t  z;
+} distance_output;
 
 extern void position_print (position_datatype *);
 extern void position_data_encode (uint8_t *, size_t *, uint8_t *, size_t *);
@@ -36,4 +48,5 @@ extern void distance_data_decode (uint8_t *, size_t *, uint8_t *, size_t *);
 
 // Guard Provisioning calls to be added here
 }
+
 #endif
