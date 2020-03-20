e8c76985554f645cb7ed19c31b5104584c83e46f
Ta Chen <tchen@perspectalabs.com>
2020-03-20 16:27:45 -0400

    removed unnecessary updates

--- apps/6month-demo/green_ownship.cpp
+++ apps/6month-demo/green_ownship.cpp
@@ -7,17 +7,6 @@
 
 // Depending on whether the subject is same or diff color, update may be local or xd
 void OwnShip::update(Subject *s) {
-  static int cnt = 0;
-  GpsSensor *gps = dynamic_cast<GpsSensor *>(s);
-  if (gps) {
-    setPosition(gps->getPosition());
-    //setVelocity(gps->getVelocity());
-    return;
-  }
-  if(_cycle != 0 && 0 == ++cnt % _cycle) {
-    print_track();
-    notify();
-  }
 }
 
 void OwnShipShadow::update(Subject *s) {
