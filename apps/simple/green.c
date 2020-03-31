ebc186a8449cc167900e552d0d8e4fd71a9d3428
Ta Chen <tchen@perspectalabs.com>
2020-03-31 09:53:26 -0400

    modified semantics of cmd line arguments

--- apps/simple/green.c
+++ apps/simple/green.c
@@ -9,140 +9,149 @@
 
 void *ctx;
 void *send_socket = NULL;
-int delay_in_ms = 1000;
+int delay_in_ms = 10;
 
 void *hal_init()
 {
-  void *ctx = xdc_ctx();
-  xdc_set_out((char *)"ipc:///tmp/halpubbwgreen");
-  xdc_set_in((char *)"ipc:///tmp/halsubbwgreen");
-    
-  xdc_register(position_data_encode, position_data_decode, DATA_TYP_POSITION);
-  xdc_register(distance_data_encode, distance_data_decode, DATA_TYP_DISTANCE);
+    void *ctx = xdc_ctx();
+    xdc_set_out((char *)"ipc:///tmp/halpubbwgreen");
+    xdc_set_in((char *)"ipc:///tmp/halsubbwgreen");
 
-  return ctx;
+    xdc_register(position_data_encode, position_data_decode, DATA_TYP_POSITION);
+    xdc_register(distance_data_encode, distance_data_decode, DATA_TYP_DISTANCE);
+
+    return ctx;
 }
 
 void *green_send(void *args)
 {
-  position_datatype pos;
-  pos.x = 1;
-  pos.y = 1;
-  pos.z = 1;
+    position_datatype pos;
+    pos.x = 1;
+    pos.y = 1;
+    pos.z = 1;
 
-  printf("creating send thread\n");
-  int count = 0;
+    printf("creating send thread\n");
+    int count = 0;
 
-  while (1) {
-    pos.trailer.seq = 1;
-    pos.trailer.rqr = 1;
-    pos.trailer.oid = 1;
-    pos.trailer.mid = 1;
-    pos.trailer.crc = 1;
+    while (1) {
+        pos.trailer.seq = 1;
+        pos.trailer.rqr = 1;
+        pos.trailer.oid = 1;
+        pos.trailer.mid = 1;
+        pos.trailer.crc = 1;
 
-    gaps_tag  t_tag;
-    uint32_t  t_mux = 1, t_sec = 1, type = DATA_TYP_POSITION;
+        gaps_tag  t_tag;
+        uint32_t  t_mux = 1, t_sec = 1, type = DATA_TYP_POSITION;
 
-    tag_write(&t_tag, t_mux, t_sec, type);
+        tag_write(&t_tag, t_mux, t_sec, type);
 
-    if (send_socket == NULL)
-      send_socket = xdc_pub_socket(ctx);
-    xdc_asyn_send(send_socket, &pos, t_tag);
+        if (send_socket == NULL)
+            send_socket = xdc_pub_socket(ctx);
+        xdc_asyn_send(send_socket, &pos, t_tag);
 
-    printf("sent %6d: (%6.0f, %6.0f, %6.0f)\n", count, pos.x, pos.y, pos.z);
-    count++;
-    
-    usleep(delay_in_ms * 1000);
+        printf("sent %6d: (%6.0f, %6.0f, %6.0f)\n", count, pos.x, pos.y, pos.z);
+        count++;
 
-    pos.x += 2;
-    pos.y += 2;
-    pos.z += 2;
-  }
+        usleep(delay_in_ms * 1000);
 
-  return NULL;
+        pos.x += 2;
+        pos.y += 2;
+        pos.z += 2;
+    }
+
+    return NULL;
 }
 
 void *green_recv_distance()
 {
-  printf("creating distance receive thread\n");
+    printf("creating distance receive thread\n");
 
-  gaps_tag t_tag;
-  uint32_t t_mux = 2, t_sec = 2, type = DATA_TYP_DISTANCE;
+    gaps_tag t_tag;
+    uint32_t t_mux = 2, t_sec = 2, type = DATA_TYP_DISTANCE;
 
-  tag_write(&t_tag, t_mux, t_sec, type);
-  void *socket = xdc_sub_socket(t_tag);
+    tag_write(&t_tag, t_mux, t_sec, type);
+    void *socket = xdc_sub_socket(t_tag);
 
-  distance_datatype dis;
+    distance_datatype dis;
 
-  int count = 0;
-  while (1) {
-    xdc_blocking_recv(socket, &dis, &t_tag);
+    int count = 0;
+    while (1) {
+        xdc_blocking_recv(socket, &dis, &t_tag);
 
-    printf("\t\t\t\t\t\trecv dis %6d: (%6.0f, %6.0f, %6.0f)\n", count, dis.x, dis.y, dis.z);
+        printf("\t\t\t\t\t\trecv dis %6d: (%6.0f, %6.0f, %6.0f)\n", count, dis.x, dis.y, dis.z);
 
-    count++;
-  }
-  zmq_close(socket);
+        count++;
+    }
+    zmq_close(socket);
 }
 
 void *green_recv_position()
 {
-  printf("creating position receive thread\n");
+    printf("creating position receive thread\n");
 
-  gaps_tag t_tag;
-  uint32_t t_mux = 2, t_sec = 2, type = DATA_TYP_POSITION;
-  
-  tag_write(&t_tag, t_mux, t_sec, type);
-  void *socket = xdc_sub_socket(t_tag);
+    gaps_tag t_tag;
+    uint32_t t_mux = 2, t_sec = 2, type = DATA_TYP_POSITION;
 
-  position_datatype pos;
+    tag_write(&t_tag, t_mux, t_sec, type);
+    void *socket = xdc_sub_socket(t_tag);
 
-  int count = 0;
-  while (1) {
-    xdc_blocking_recv(socket, &pos, &t_tag);
+    position_datatype pos;
 
-    printf("\t\t\t\t\t\trecv pos %6d: (%6.0f, %6.0f, %6.0f)\n", count, pos.x, pos.y, pos.z);
+    int count = 0;
+    while (1) {
+        xdc_blocking_recv(socket, &pos, &t_tag);
 
-    count++;
-  }
-  zmq_close(socket);
+        printf("\t\t\t\t\t\trecv pos %6d: (%6.0f, %6.0f, %6.0f)\n", count, pos.x, pos.y, pos.z);
+
+        count++;
+    }
+    zmq_close(socket);
 }
 
 int main(int argc, char **argv)
 {
-  if (argc > 1) {
-    delay_in_ms = atoi(argv[1]);
-    if (delay_in_ms <= 0) {
-      printf("bad argument %s\n", argv[1]);
-      exit(1);
+    if (argc != 1 && argc != 2) {
+        printf("Usage: %s <Hz>\n\
+\t where <Hz> is the frequency in sending positions,\n\
+\t with default 100 Hz\n", argv[0]);
+        exit(1);
     }
-  }
+    else if (argc == 2) {
+        int hertz = atoi(argv[1]);
 
-  printf("green %d\n", delay_in_ms);
-  ctx = hal_init();
+        if (hertz <= 0) {
+            printf("bad argument %s\n", argv[1]);
+            exit(1);
+        }
 
-  pthread_t sendThread, recvDisThread, recvPosThread;
+        delay_in_ms = (int) (1000 / (double)hertz);
+    }
 
-  int rtn = pthread_create(&sendThread, NULL, &green_send, NULL);
-  if (rtn != 0) {
-    printf("send thread creat failed\n");
-    exit(1);
-  }
+    printf("green %d\n", delay_in_ms);
+    ctx = hal_init();
 
-  rtn = pthread_create(&recvDisThread, NULL, &green_recv_distance, NULL);
-  if (rtn != 0) {
-    printf("receice thread creat failed\n");
-    exit(1);
-  }
+    pthread_t sendThread, recvDisThread, recvPosThread;
+
+    int rtn = pthread_create(&sendThread, NULL, &green_send, NULL);
+    if (rtn != 0) {
+        printf("send thread creat failed\n");
+        exit(1);
+    }
+
+    rtn = pthread_create(&recvDisThread, NULL, &green_recv_distance, NULL);
+    if (rtn != 0) {
+        printf("receice thread creat failed\n");
+        exit(1);
+    }
 
     rtn = pthread_create(&recvPosThread, NULL, &green_recv_position, NULL);
-  if (rtn != 0) {
-    printf("receice thread creat failed\n");
-    exit(1);
-  }
+    if (rtn != 0) {
+        printf("receice thread creat failed\n");
+        exit(1);
+    }
 
-  pthread_join(sendThread, NULL);
-  pthread_join(recvDisThread, NULL);
-  pthread_join(recvPosThread, NULL);    
+    pthread_join(sendThread, NULL);
+    pthread_join(recvDisThread, NULL);
+    pthread_join(recvPosThread, NULL);
 }
 
--- apps/simple/orange.c
+++ apps/simple/orange.c
@@ -8,164 +8,173 @@
 #include "gma.h"
 
 void *ctx;
-int delay_in_ms_dis = 1000;
-int delay_in_ms_pos = 1000;
+int delay_in_ms_dis = 10;
+int delay_in_ms_pos = 100;
 
 void *hal_init()
 {
-  void *ctx = xdc_ctx();
-  xdc_set_out((char *)"ipc:///tmp/halpubbworange");
-  xdc_set_in((char *)"ipc:///tmp/halsubbworange");
+    void *ctx = xdc_ctx();
+    xdc_set_out((char *)"ipc:///tmp/halpubbworange");
+    xdc_set_in((char *)"ipc:///tmp/halsubbworange");
 
-  xdc_register(position_data_encode, position_data_decode, DATA_TYP_POSITION);
-  xdc_register(distance_data_encode, distance_data_decode, DATA_TYP_DISTANCE);
+    xdc_register(position_data_encode, position_data_decode, DATA_TYP_POSITION);
+    xdc_register(distance_data_encode, distance_data_decode, DATA_TYP_DISTANCE);
 
-  return ctx;
+    return ctx;
 }
 
 void *orange_send_distance(void *args)
 {
-  distance_datatype dis;
-  dis.x = 2;
-  dis.y = 2;
-  dis.z = 2;
+    distance_datatype dis;
+    dis.x = 2;
+    dis.y = 2;
+    dis.z = 2;
 
-  printf("creating send thread\n");
-  int count = 0;
+    printf("creating send thread\n");
+    int count = 0;
 
-  void *send_socket = xdc_pub_socket(ctx);
+    void *send_socket = xdc_pub_socket(ctx);
 
-  while (1) {
-    usleep(delay_in_ms_dis * 1000);
+    while (1) {
+        usleep(delay_in_ms_dis * 1000);
 
-    dis.trailer.seq = 2;
-    dis.trailer.rqr = 2;
-    dis.trailer.oid = 2;
-    dis.trailer.mid = 2;
-    dis.trailer.crc = 2;
+        dis.trailer.seq = 2;
+        dis.trailer.rqr = 2;
+        dis.trailer.oid = 2;
+        dis.trailer.mid = 2;
+        dis.trailer.crc = 2;
 
-    gaps_tag  t_tag;
-    uint32_t  t_mux = 2, t_sec = 2, type = DATA_TYP_DISTANCE;
+        gaps_tag  t_tag;
+        uint32_t  t_mux = 2, t_sec = 2, type = DATA_TYP_DISTANCE;
 
-    tag_write(&t_tag, t_mux, t_sec, type);
+        tag_write(&t_tag, t_mux, t_sec, type);
 
-    xdc_asyn_send(send_socket, &dis, t_tag);
+        xdc_asyn_send(send_socket, &dis, t_tag);
 
-    printf("sent dis %6d: (%6.0f, %6.0f, %6.0f)\n", count, dis.x, dis.y, dis.z);
-    count++;
-    
-    dis.x += 2;
-    dis.y += 2;
-    dis.z += 2;
-  }
+        printf("sent dis %6d: (%6.0f, %6.0f, %6.0f)\n", count, dis.x, dis.y, dis.z);
+        count++;
 
-  return NULL;
+        dis.x += 2;
+        dis.y += 2;
+        dis.z += 2;
+    }
+
+    return NULL;
 }
 
 void *orange_send_position(void *args)
 {
-  position_datatype pos;
-  pos.x = 1;
-  pos.y = 1;
-  pos.z = 1;
+    position_datatype pos;
+    pos.x = 1;
+    pos.y = 1;
+    pos.z = 1;
 
-  printf("creating send thread\n");
-  int count = 0;
-  void *send_socket = xdc_pub_socket(ctx);
+    printf("creating send thread\n");
+    int count = 0;
+    void *send_socket = xdc_pub_socket(ctx);
 
-  while (1) {
-    usleep(delay_in_ms_pos * 1000);
+    while (1) {
+        usleep(delay_in_ms_pos * 1000);
 
-    pos.trailer.seq = 1;
-    pos.trailer.rqr = 1;
-    pos.trailer.oid = 1;
-    pos.trailer.mid = 1;
-    pos.trailer.crc = 1;
+        pos.trailer.seq = 1;
+        pos.trailer.rqr = 1;
+        pos.trailer.oid = 1;
+        pos.trailer.mid = 1;
+        pos.trailer.crc = 1;
 
-    gaps_tag  t_tag;
-    uint32_t  t_mux = 2, t_sec = 2, type = DATA_TYP_POSITION;
+        gaps_tag  t_tag;
+        uint32_t  t_mux = 2, t_sec = 2, type = DATA_TYP_POSITION;
 
-    tag_write(&t_tag, t_mux, t_sec, type);
+        tag_write(&t_tag, t_mux, t_sec, type);
 
-    xdc_asyn_send(send_socket, &pos, t_tag);
+        xdc_asyn_send(send_socket, &pos, t_tag);
 
-    printf("sent pos %6d: (%6.0f, %6.0f, %6.0f)\n", count, pos.x, pos.y, pos.z);
-    count++;
-    
-    pos.x += 2;
-    pos.y += 2;
-    pos.z += 2;
-  }
+        printf("sent pos %6d: (%6.0f, %6.0f, %6.0f)\n", count, pos.x, pos.y, pos.z);
+        count++;
 
-  return NULL;
+        pos.x += 2;
+        pos.y += 2;
+        pos.z += 2;
+    }
+
+    return NULL;
 }
 
 void *orange_recv()
 {
-  printf("creating receive thread\n");
+    printf("creating receive thread\n");
 
-  gaps_tag t_tag;
-  uint32_t  t_mux = 1, t_sec = 1, type = DATA_TYP_POSITION;
+    gaps_tag t_tag;
+    uint32_t  t_mux = 1, t_sec = 1, type = DATA_TYP_POSITION;
 
-  tag_write(&t_tag, t_mux, t_sec, type);
-  void *socket = xdc_sub_socket(t_tag);
+    tag_write(&t_tag, t_mux, t_sec, type);
+    void *socket = xdc_sub_socket(t_tag);
 
-  position_datatype pos;
+    position_datatype pos;
 
-  int count = 0;
-  while (1) {
-    xdc_blocking_recv(socket, &pos, &t_tag);
+    int count = 0;
+    while (1) {
+        xdc_blocking_recv(socket, &pos, &t_tag);
 
-    printf("\t\t\t\t\t\trecv %6d: (%6.0f, %6.0f, %6.0f)\n", count, pos.x, pos.y, pos.z);
+        printf("\t\t\t\t\t\trecv %6d: (%6.0f, %6.0f, %6.0f)\n", count, pos.x, pos.y, pos.z);
 
-    count++;
-  }
-  zmq_close(socket);
+        count++;
+    }
+    zmq_close(socket);
 
-  return NULL;
+    return NULL;
 }
 
 int main(int argc, char **argv)
 {
-  if (argc >= 3) {
-    delay_in_ms_dis = atoi(argv[1]);
-    if (delay_in_ms_dis <= 0) {
-      printf("bad argument %s\n", argv[1]);
-      exit(1);
+    if (argc != 1 && argc != 3) {
+        printf("Usage: %s <Distance Hz> <Position Hz>\n\
+\t where <Distance Hz> and <Position Hz> are the frequencies\n\
+\t in sending distances and positions with defaults at 100 and 10 Hz, respectively.\n",
+                argv[0]);
+        exit(1);
+    }
+    else if (argc == 3) {
+        int hertz = atoi(argv[1]);
+        if (hertz <= 0) {
+            printf("bad argument %s\n", argv[1]);
+            exit(1);
+        }
+        delay_in_ms_dis = (int) (1000 / (double) hertz);
+
+        hertz = atoi(argv[2]);
+        if (hertz <= 0) {
+            printf("bad argument %s\n", argv[1]);
+            exit(1);
+        }
+        delay_in_ms_pos = (int) (1000 / (double) hertz);
     }
 
-    delay_in_ms_pos = atoi(argv[2]);
-    if (delay_in_ms_pos <= 0) {
-      printf("bad argument %s\n", argv[2]);
-      exit(1);
+    printf("orange %d %d\n", delay_in_ms_dis, delay_in_ms_pos);
+    ctx = hal_init();
+
+    pthread_t sendDisThread, sendPosThread, recvThread;
+
+    int rtn = pthread_create(&sendDisThread, NULL, &orange_send_distance, NULL);
+    if (rtn != 0) {
+        printf("send distance thread creat failed\n");
+        exit(1);
     }
-  }
 
-  printf("orange %d %d\n", delay_in_ms_dis, delay_in_ms_pos);
-  ctx = hal_init();
+    rtn = pthread_create(&sendPosThread, NULL, &orange_send_position, NULL);
+    if (rtn != 0) {
+        printf("send position thread creat failed\n");
+        exit(1);
+    }
 
-  pthread_t sendDisThread, sendPosThread, recvThread;
-  
-  int rtn = pthread_create(&sendDisThread, NULL, &orange_send_distance, NULL);
-  if (rtn != 0) {
-    printf("send distance thread creat failed\n");
-    exit(1);
-  }
-  
-  rtn = pthread_create(&sendPosThread, NULL, &orange_send_position, NULL);
-  if (rtn != 0) {
-    printf("send position thread creat failed\n");
-    exit(1);
-  }
-  
-  rtn = pthread_create(&recvThread, NULL, &orange_recv, NULL);
-  if (rtn != 0) {
-    printf("receice thread creat failed\n");
-    exit(1);
-  }
+    rtn = pthread_create(&recvThread, NULL, &orange_recv, NULL);
+    if (rtn != 0) {
+        printf("receice thread creat failed\n");
+        exit(1);
+    }
 
-  pthread_join(sendDisThread, NULL);
-  pthread_join(sendPosThread, NULL);
-  pthread_join(recvThread, NULL);  
+    pthread_join(sendDisThread, NULL);
+    pthread_join(sendPosThread, NULL);
+    pthread_join(recvThread, NULL);
 }
 
