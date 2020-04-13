af831b81b8cc8cd4602a76517388a4b1c96a6175
Ta Chen <tchen@perspectalabs.com>
2020-04-13 09:09:58 -0400

    remove PL comments

--- apps/6month-demo/orange.cpp
+++ apps/6month-demo/orange.cpp
@@ -1,13 +1,11 @@
 // orange.cpp : Defines the entry point for the console application. (orange enclave)
 //
-#include <signal.h>
+
 #include <iostream>
-#include <zmq.h>
 #ifdef _WIN32
 # include <Windows.h>
 #else
 # include <unistd.h>
-# include <string.h>
 #endif
 #include "pnt_data.h"
 #include "sensors.h"
@@ -37,14 +35,10 @@ int main(int argc, char **argv)
    std::cout << "orange " << std::endl;
    hal_init();
 
-  // Assume the color for p, d, v, vtgt is inferred from below coloring in constructors
-  // Touched on green side gpssensor constructor
   Position p(.0, .0, .0); // initial position
-  // Touched on orange side by RFSensor constructor
   Distance d(1062, 7800, 9000); // initial target distance
-  // Touched by green side gpssensor constructor
+
   Velocity v(50, 25, 12);
-  // touched by orange side RFSensor constructor
   Velocity vtgt(35, 625, 18);
   GpsSensorShadow* gps = new GpsSensorShadow(p, v);
 
@@ -56,11 +50,10 @@ int main(int argc, char **argv)
   TargetShadow* tgt = new TargetShadow (10); // updates at 10 Hz frequency
 
   // setup the dataflow relationships
-  gps->attach(uav); // cross domain attach gps is green uav is orange
+  gps->attach(uav);
   gps->attach(tgt);
-  uav->attach(tgt); // cross domain 
-  rfs->attach(tgt); // cross domain 
-  // _observers may be tained; contains a mix of local and remote observers; kind of split TBD
+  uav->attach(tgt);
+  rfs->attach(tgt);
 
   while (true)
     {
