33551a29aeebd1d0bfbaacdf7c30f8bf97339f1a
Rajesh Krishnan <rkrishnan@perspectalabs.com>
2020-10-07 18:48:32 -0400

    xdcc expanded out to allow annotations

--- apps/eop1/xdcc/sample-xdcc/xdcc_skeleton.c
+++ apps/eop1/xdcc/sample-xdcc/xdcc_skeleton.c
@@ -8,44 +8,83 @@
 amqlib_t *amq() { static amqlib_t *a = NULL; if (a == NULL) { a = amqlib_create(); } return a; }
 
 /* Messages in system */
-#define ALL_AMQ_MSG_LST \
+#define ALL_MSGS_LIST \
   component_heartbeats, \
   updateMissionPlan, \
   updateNewTasking, \
-  requestISRMDetections, \
+  requestISRMDetections,\
   recieveISRMDetections, \
   groundMovers, \
   pnt
 
-/* Remote messages if any */
-/* #define REMOTE_MSG_LST */
+/* _local_X is 1 if X is local, else 0 */
+#define _local_component_heartbeats 1
+#define _local_updateMissionPlan 1
+#define _local_updateNewTasking 1
+#define _local_requestISRMDetections 1
+#define _local_recieveISRMDetections 1
+#define _local_groundMovers 1
+#define _local_pnt 1
 
-/* Messages destined not to topics, but to queues */
-/* #define QUEUE_MSG_LST */
+/* _topic_X is 1 if X is a topic, else 0 */
+#define _topic_component_heartbeats 1
+#define _topic_updateMissionPlan 1
+#define _topic_updateNewTasking 1
+#define _topic_requestISRMDetections 1
+#define _topic_recieveISRMDetections 1
+#define _topic_groundMovers 0
+#define _topic_pnt 1
 
-#define LOCAL(X)   int _local_##X = 1;
-#define UNLOCAL(X) _local_##X = 0;
-MAP(LOCAL,   ALL_AMQ_MSG_LST)
-#ifdef REMOTE_MSG_LST
-MAP(UNLOCAL, REMOTE_MSG_LST)
-#endif
+/* XXX: Some of these echo_functions will need to be blessed using CLE for RPC wrapping */
+void echo_component_heartbeats(const char *s) {
+  amqlib_publish(amq(), "component_heartbeats", s, _topic_component_heartbeats);
+}
+void echo_updateMissionPlan(const char *s) {
+  amqlib_publish(amq(), "updateMissionPlan", s, _topic_updateMissionPlan);
+}
+void echo_updateNewTasking(const char *s) {
+  amqlib_publish(amq(), "updateNewTasking", s, _topic_updateNewTasking);
+}
+void echo_requestISRMDetections(const char *s) {
+  amqlib_publish(amq(), "requestISRMDetections", s, _topic_requestISRMDetections);
+}
+void echo_recieveISRMDetections(const char *s) {
+  amqlib_publish(amq(), "recieveISRMDetections", s, _topic_recieveISRMDetections);
+}
+void echo_groundMovers(const char *s) {
+  amqlib_publish(amq(), "groundMovers", s, _topic_groundMovers);
+}
+void echo_pnt(const char *s) {
+  amqlib_publish(amq(), "pnt", s, _topic_pnt);
+}
 
-#define TOPIC(X)   int _topic_##X = 1;
-#define UNTOPIC(X) _topic_##X = 0;
-MAP(TOPIC,   ALL_AMQ_MSG_LST)
-#ifdef QUEUE_MSG_LST
-MAP(UNTOPIC, QUEUE_MSG_LST)
-#endif
-
-/* XXX: Need to add CLE annotations to the functions defined by this macro */
-#define XDCCEGRESS(X) void egress_##X(const char *s) { if (!_local_##X) amqlib_publish(amq(), #X, s, _topic_##X); }
-MAP(XDCCEGRESS, ALL_AMQ_MSG_LST)
+/* XXX: Some echo_ functions will be moved to remote enclave, and corresponding _rpc_echo_ functions created and called below */
+void egress_component_heartbeats(const char *s) {
+  if(!_local_component_heartbeats) echo_component_heartbeats(s);
+}
+void egress_updateMissionPlan(const char *s) {
+  if(!_local_updateMissionPlan) echo_updateMissionPlan(s);
+}
+void egress_updateNewTasking(const char *s) {
+  if(!_local_updateNewTasking) echo_updateNewTasking(s);
+}
+void egress_requestISRMDetections(const char *s) {
+  if(!_local_requestISRMDetections) echo_requestISRMDetections(s);
+}
+void egress_recieveISRMDetections(const char *s) {
+  if(!_local_recieveISRMDetections) echo_recieveISRMDetections(s);
+}
+void egress_groundMovers(const char *s) {
+  if(!_local_groundMovers) echo_groundMovers(s);
+}
+void egress_pnt(const char *s) {
+  if(!_local_pnt) echo_pnt(s);
+}
 
 #define XDCCLISTEN(X) amqlib_listen(amq(), #X, egress_##X, _topic_##X); 
-  
 int main() { 
   amq();
-  MAP(XDCCLISTEN, ALL_AMQ_MSG_LST)
+  MAP(XDCCLISTEN, ALL_MSGS_LIST)
   while(1) { sleep(100); }
   amqlib_destroy(amq());
   return 0; 
