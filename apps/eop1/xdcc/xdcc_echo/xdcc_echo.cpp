227537a4f870cabfdb209425581886b5e07f53de
Michael Kaplan <mkaplan@perspectalabs.com>
2020-11-30 17:37:32 -0500

    c++ publishes json

--- apps/eop1/xdcc/xdcc_echo/xdcc_echo.cpp
+++ apps/eop1/xdcc/xdcc_echo/xdcc_echo.cpp
@@ -13,36 +13,31 @@ void echo_component_heartbeats_cpp(
     amqlib_t *__amqlib,
     int __is_topic,
     const char *ss,
-    const char *st,
-    int k)
+    const char *st)
 {
     json js;
     js["fromRemote"] = 1;
     js["ss"] = string(ss);
     js["st"] = string(st);
-    js["k"]  = k;
 
     AMQManager *amq = static_cast<AMQManager *>(__amqlib->obj);
-    amq->publish("component_heartbeats", js.dump(), __is_topic==0?false:true);
+    amq->publish("component_heartbeats", js, __is_topic==0?false:true);
 }
 
 void unmarshal_component_heartbeats(
     const char *s,
     int *fromRemote, 
     char *ss, 
-    char *st, 
-    int *k)
+    char *st)
 {
     json js = json::parse(s);
     int fromRemote_cpp = js.contains("fromRemote")?js["fromRemote"].get<int>():0;
     string ss_cpp = js["ss"].get<string>();
     string st_cpp = js["st"].get<string>();
-    int k_cpp     = js["k"].get<int>();
 
     *fromRemote = fromRemote_cpp;
     strncpy(ss, ss_cpp.c_str(),MAXSTR_SIZE);
     strncpy(st, st_cpp.c_str(),MAXSTR_SIZE);
-    *k = k_cpp;
 }
 
 } /* extern "C" */
