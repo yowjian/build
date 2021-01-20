8e8f4266ba2be8da80bdd8ef3a24ad6ae87dfb11
Michael Kaplan <mkaplan@perspectalabs.com>
2021-01-20 13:25:03 -0500

    changes on deps handling

--- apps/eop1/case3/MA_v1.0_src/package.sh
+++ apps/eop1/case3/MA_v1.0_src/package.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tar zcf MA_1.0.tgz EOIR External include ISRM MPU MPX RDR scripts TestMA CMakeLists.txt gaps.ma.dependencies/deps --dereference
+tar zcf MA_1.0.tgz EOIR External include ISRM ISRMshadow MPU MPX RDR scripts TestMA CMakeLists.txt gaps.ma.dependencies.tgz --dereference
