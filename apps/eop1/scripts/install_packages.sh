8dc0dc79e531bcad04d8453986563b9fe1fcf136
mkaplan <mkaplan@appcomsci.com>
2021-02-03 11:39:40 -0500

    set py packages versions

--- apps/eop1/scripts/install_packages.sh
+++ apps/eop1/scripts/install_packages.sh
@@ -2,4 +2,4 @@
 
 sudo apt install gcc make git libz3-dev libzmq3-dev libconfig-dev python3-pip libapr1 libapr1-dev nasm yasm cmake autoconf automake libtool libssl-dev openjdk-14-jre-headless
 
-sudo -H pip3 install decorator==4.4.1 jsonschema lark-parser==0.7.8 libconf vstask
+sudo -H pip3 install decorator==4.4.1 jsonschema==3.2.0 lark-parser==0.7.8 libconf==2.0.1 vstask==1.1.5
