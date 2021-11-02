7cb1d2c142c31db1301003f34795c27843a7e570
Benjamin Flin <benjamin.flin@peratonlabs.com>
2021-11-02 17:41:41 +0000

    updated example1

--- apps/examples/example1/.vscode/closure_env.sh
+++ apps/examples/example1/.vscode/closure_env.sh
@@ -2,41 +2,36 @@
 export PROG=$(eval basename `pwd`)
 export PYTHON="/usr/bin/python3 -u"
 
-CLOSURE_TOOLS=/opt/closure
+CLOSURE_TOOLS=/home/bflin/gaps/build/test-install
 export CLOSURE_BINS=${CLOSURE_TOOLS}/bin
 export CLOSURE_INCLUDES=${CLOSURE_TOOLS}/include
 export CLOSURE_LIBS=${CLOSURE_TOOLS}/lib
-export CLOSURE_SCRIPTS=${CLOSURE_TOOLS}/scripts
+export CLOSURE_PYTHON=${CLOSURE_TOOLS}/python
 export CLOSURE_SCHEMAS=${CLOSURE_TOOLS}/schemas
 export CLOSURE_DEVICES=${CLOSURE_TOOLS}/etc
 
-export OPT_DBG=${CLOSURE_BINS}/opt-debug
-export OPT_NODBG=/usr/local/bin/opt
+export OPT=/usr/local/bin/opt
 export VERIFIER=${CLOSURE_BINS}/verifier
 
 export LIBPDG=${CLOSURE_LIBS}/libpdg.so
 export LIBGEDL=${CLOSURE_LIBS}/libgedl.so
 
-export PREPROCESSOR=${CLOSURE_SCRIPTS}/qd_cle_preprocessor.py
-export JOINCLEMAPS=${CLOSURE_SCRIPTS}/join_clemaps.py
-export PARTITIONER=${CLOSURE_SCRIPTS}/partitioner.py
-export TAGPROC=${CLOSURE_SCRIPTS}/tag_processor.py
-export CUTZOOM=${CLOSURE_SCRIPTS}/cutzoom.py
-export DIV=${CLOSURE_SCRIPTS}/program_divider.py
-export IDLGENERATOR=${CLOSURE_SCRIPTS}/IDLGenerator.py
-export RPCGENERATOR=${CLOSURE_SCRIPTS}/RPCGenerator_rk.py
-export AUTOGEN=${CLOSURE_SCRIPTS}/autogen.py
-export HALGEN=${CLOSURE_SCRIPTS}/hal_autoconfig.py
+export PREPROCESSOR=preprocessor
+export JOINCLEMAPS=join_clemaps
+export PARTITIONER=partitioner
+# only used in cutzoom
+export DIV=divider
+export IDLGENERATOR=idl_generator
+export RPCGENERATOR=rpc_generator
+export AUTOGEN=hal_autogen
+export HALGEN=hal_autoconfig.py
 
-LLVM_RELEASE=/usr/local
-export CLANG=${LLVM_RELEASE}/bin/clang-10
+export CLANG=clang
 export CLANG_FLAGS="-S -g -emit-llvm"
-export LLVMLINK=${LLVM_RELEASE}/bin/llvm-link
-export LLVMDIS=${LLVM_RELEASE}/bin/llvm-dis
-
-export LLVM_DIR=${LLVM_RELEASE}
+export LLVMLINK=llvm-link
+export LLVMDIS=llvm-dis
 
 export HWMODE=emu
 export IPCMODE=multithreaded
-export ENCLAVES="orange purple"
+export ENCLAVES="orange_E purple_E"
 export EXT=c
--- apps/examples/example1/.vscode/join.sh
+++ apps/examples/example1/.vscode/join.sh
@@ -3,4 +3,4 @@
 ODIR=$1
 OUTFILE=$2
 INFILES=`find $ODIR -name *.[ch]`
-python3 /opt/closure/scripts/join_clemaps.py $OUTFILE $INFILES
+join_clemaps $OUTFILE $INFILES
