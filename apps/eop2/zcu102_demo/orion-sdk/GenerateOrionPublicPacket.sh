cd67680ab2874b0b3ebbb5eb75b816f7e3567950
Rajesh Krishnan <rkrishnan@perspectalabs.com>
2022-07-08 07:46:06 -0400

    moved to separate repo for petalinux build

--- apps/eop2/zcu102_demo/orion-sdk/GenerateOrionPublicPacket.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-ROOT_DIR=`dirname $0`
-
-$ROOT_DIR/Protogen/Protogen.sh $ROOT_DIR/Communications/OrionPublicProtocol.xml $ROOT_DIR/Communications -no-doxygen
-
--- apps/eop2/zcu102_demo/orion-sdk/GenerateOrionPublicPacketWin.bat
+++
@@ -1,3 +0,0 @@
-set ROOT=%~dp0
-"%ROOT%\Protogen\Windows\ProtoGen.exe" "%ROOT%\Communications\OrionPublicProtocol.xml" "%ROOT%\Communications" -no-doxygen
-exit /b 0
