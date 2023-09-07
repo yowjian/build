6dd41c5edd5214076f437aadf7e3d49e7a08ecff
Michael Kaplan <mkaplan@perspectalabs.com>
2023-09-07 21:14:24 +0000

    changes with pinstall in container

--- apps/eop1/scripts/build_cases.sh
+++ apps/eop1/scripts/build_cases.sh
@@ -28,7 +28,8 @@ handle_opts() {
   shift "$((OPTIND-1))"
 
   PWD=`pwd`
-  PINSTALL="${PWD}/../case1/MA_v1.0_src/pinstall"
+  #PINSTALL="${PWD}/../case1/MA_v1.0_src/pinstall"
+  PINSTALL="/opt/eop1/pinstall"
 
   case $HWMODE in
       ilip)
